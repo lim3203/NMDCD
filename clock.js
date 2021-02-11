2f1c4ace76758e0e74bd039d5b494ce95cc2774f
lim3203 <sunglim3203@gmail.com>
2021-02-11 19:08:25 +0900

    ㅎㅎ
    ㅎㅎ

--- clock.js
+++ clock.js
@@ -1,17 +1,27 @@
 const clockContainer = document.querySelector(".js-clock");
 const clockTitle = clockContainer.querySelector("h1");
+const amPm = document.querySelector(".amPm");
+const amPmTitle = amPm.querySelector("h4");
 
 function getTime() {
     const date = new Date();
     const minutes = date.getMinutes();
-    const hours = date.getHours();
+    let hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${
+    if(hours >= 12){
+        hours = hours - 12;
+        amPmTitle.innerText = `pm`;
+    }   else   {
+        if(hours < 10){
+            hours = '0$hours';
+            amPm.innerText = `am`;
+        }
+    }
+    clockTitle.innerText = `${hours}:${
         minutes <10 ? `0${minutes}` : minutes}:${
         seconds < 10 ? `0${seconds}` : seconds
     }`;
 }
-
 
 
 
@@ -21,4 +31,3 @@ function init() {
 }
 
 init();
-
